72512b08f991faf8eaad0defd2696043fe7e9ed3
Alexey Loginov <alekseyloginov79@gmail.com>
2023-10-19 07:17:22 +0300

    Добавлены тесты и обновлены настройки Jest

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -14,4 +14,32 @@ describe("Books names test suit", () => {
       "Гарри Поттер",
     ]);
   });
+
+  it("Should sort names with different cases", () => {
+    expect(
+      sorting.sortByName(["Гарри Поттер", "гАРРИ пОТТЕР", "ГАРРИ ПОТТЕР"])
+    ).toEqual(["Гарри Поттер", "гАРРИ пОТТЕР", "ГАРРИ ПОТТЕР"]);
+  });
+
+  it("Should sort names that start with the same letters but different cases", () => {
+    expect(
+      sorting.sortByName(["Властелин Колец", "властелин Колец", "Властелин колец"])
+    ).toEqual(["Властелин Колец", "властелин Колец", "Властелин колец"]);
+  });
+
+  it("Should handle names that are exactly the same but with different cases", () => {
+    expect(
+      sorting.sortByName(["Волшебник изумрудного города", "ВОЛШЕБНИК ИЗУМРУДНОГО ГОРОДА", "ВОЛШЕБНИК ИЗУМРУДНОГО ГОРОДА"])
+    ).toEqual(["Волшебник изумрудного города", "ВОЛШЕБНИК ИЗУМРУДНОГО ГОРОДА", "ВОЛШЕБНИК ИЗУМРУДНОГО ГОРОДА"]);
+  });
+
+  it("Should handle an empty array", () => {
+    expect(sorting.sortByName([])).toEqual([]);
+  });
+
+  it("Should handle an array with a single element", () => {
+    expect(sorting.sortByName(["Гарри Поттер"])).toEqual(["Гарри Поттер"]);
+  });
 });
+
+
